832bf664a085fe6740748fb9ce68fd63b9ca1ad6
caetano borba peruzzo <CBperuzzo4@gmail.com>
2025-02-04 18:53:56 -0300

    removed unused imports

--- src/main/java/com/lumem/hgest/security/SpringSecurityConfiguration.java
+++ src/main/java/com/lumem/hgest/security/SpringSecurityConfiguration.java
@@ -1,38 +1,15 @@
 package com.lumem.hgest.security;
 
-import com.lumem.hgest.model.StoredUser;
-import com.lumem.hgest.repository.StoredUserRepository;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.ProviderManagerBuilder;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.RememberMeConfigurer;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
-import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
-import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices.RememberMeTokenAlgorithm;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
-import org.springframework.security.web.server.authentication.logout.WebSessionServerLogoutHandler;
-
-import javax.xml.transform.Source;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -57,6 +34,7 @@ public class SpringSecurityConfiguration{
         http.authorizeHttpRequests(r -> {
             r.requestMatchers("/css/msgform.css","/css/form.css","/css/onehundred.css","/css/background.css",
                             "/register","/register/processing").permitAll()
+                    .requestMatchers("/isadmin").hasRole("ADMIN")
             .anyRequest().authenticated();
         });
 
