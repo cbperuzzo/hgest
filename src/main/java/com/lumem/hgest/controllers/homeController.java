d4f207aa4929ae52896e594d37a5e282063a8025
caetano borba peruzzo <CBperuzzo4@gmail.com>
2025-02-04 18:53:56 -0300

    removed unused imports

--- src/main/java/com/lumem/hgest/controllers/homeController.java
+++ src/main/java/com/lumem/hgest/controllers/homeController.java
@@ -3,11 +3,8 @@ package com.lumem.hgest.controllers;
 
 import com.lumem.hgest.model.Util.UserModelAndVeiw;
 import com.lumem.hgest.security.AuthenticationService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
--- src/main/java/com/lumem/hgest/security/AuthenticationService.java
+++ src/main/java/com/lumem/hgest/security/AuthenticationService.java
@@ -1,20 +1,19 @@
 package com.lumem.hgest.security;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import com.lumem.hgest.model.StoredUser;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
-import java.util.Collection;
-import java.util.List;
 
 @Service
 public class AuthenticationService {
 
     public StoredUser getCurrentUser(){
         return (StoredUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+    }
+
+    public StoredUser getCurrent(){
+        return (StoredUser) SecurityContextHolder.getContext().getAuthentication().getDetails();
 
     }
 
