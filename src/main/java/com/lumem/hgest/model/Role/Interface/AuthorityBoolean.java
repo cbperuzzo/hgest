ce6b76cb0d9daab6b7c8629c8ccbc42849fc8e16
caetano borba peruzzo <CBperuzzo4@gmail.com>
2025-02-04 22:44:38 -0300

    dynamically defining new authorities for roles
    WiP : has to be processed partially at preCountruct, currently it isnt

---
+++ src/main/java/com/lumem/hgest/model/Role/Interface/AuthorityBoolean.java
@@ -0,0 +1,8 @@
+package com.lumem.hgest.model.Role.Interface;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthorityBoolean {
+}
--- src/main/java/com/lumem/hgest/model/Role/RoleEnum.java
+++ src/main/java/com/lumem/hgest/model/Role/RoleEnum.java
@@ -1,5 +1,12 @@
 package com.lumem.hgest.model.Role;
 
+import com.lumem.hgest.model.Role.Interface.AuthorityBoolean;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
 public enum RoleEnum {
     SUPERVISOR("SUPERVISOR",true,false),
     WORKER("WORKER",false,false),
@@ -20,10 +27,31 @@ public enum RoleEnum {
         return name;
     }
 
+    public List<String> authorities(){
+
+        List<Method> methods = Arrays.stream(this.getClass().getMethods()).toList();
+
+        methods = methods.stream().filter(method -> method.getAnnotation(AuthorityBoolean.class) != null).toList();
+
+        methods = methods.stream().filter(method -> {
+            boolean result = false;
+            try {
+                result = (boolean) method.invoke(this);
+            } catch (IllegalAccessException | InvocationTargetException ignored) {
+
+            }
+            return result;
+        }).toList();
+
+        return methods.stream().map(method -> MethodFormater.removeFirstTwo(method.getName()).toUpperCase()).toList();
+    }
+
+    @AuthorityBoolean
     public boolean isEdit() {
         return edit;
     }
 
+    @AuthorityBoolean
     public boolean isStats() {
         return stats;
     }
@@ -35,5 +63,15 @@ public enum RoleEnum {
                 ", edit=" + edit +
                 ", stats=" + stats +
                 '}';
+    }
+}
+
+class MethodFormater{
+
+    static String removeFirstTwo(String methodName){
+        StringBuilder sb = new StringBuilder(methodName);
+        sb.deleteCharAt(0);
+        sb.deleteCharAt(0);
+        return sb.toString();
     }
 }
