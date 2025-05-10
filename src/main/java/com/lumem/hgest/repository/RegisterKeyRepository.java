0f1ee1deee927b2b7bbe5d859ee319e582f95998
caetano borba peruzzo <CBperuzzo4@gmail.com>
2025-05-10 02:40:39 -0300

    jwtutil
    removed legacy and finished jwtutil class

--- src/main/java/com/lumem/hgest/repository/RegisterKeyRepository.java
+++ src/main/java/com/lumem/hgest/repository/RegisterKeyRepository.java
@@ -4,5 +4,5 @@ import com.lumem.hgest.model.RegisterKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RegisterKeyRepository extends JpaRepository<RegisterKey,Long> {
-    boolean existsByValueAndValid(String valeu, boolean valid);
+    boolean existsByValueAndValid(String value, boolean valid);
 }
